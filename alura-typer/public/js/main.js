70d7b71ae11e045d17d8c5add87f624d3556eee3
Thaina Ribeiro <thainaribeiro.lima@outlook.com>
2019-05-13 18:14:52 -0300

    Criando função para contar caracteres e palavras inputadas no textarea

--- alura-typer/public/js/main.js
+++ alura-typer/public/js/main.js
@@ -2,3 +2,13 @@ var frase = $(".frase").text();
 var numPalavras = frase.split(" ").length;
 var tamanhoFrase = $("#quantidadePalavras");
 tamanhoFrase.text(numPalavras);
+
+var campo = $(".campoDigitacao");
+campo.on("input", function(){
+  var conteudo = campo.val();
+  var qtdePalavras = conteudo.split(/\s+/).length -1;
+  $("#contadorPalavras").text(qtdePalavras);
+
+  var qtdeCaracteres = conteudo.length;
+  $("#qtdeCaracteres").text(qtdeCaracteres);
+});
